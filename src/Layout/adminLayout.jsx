b7a2e2fa41e4c22390a867e3aa8e0e9dc4e43f1d
Ben Arafat <arafatfofana6@gmail.com>
2026-08-12 10:30:16 +0100

    Fix case-sensitive imports for Vercel

--- src/Layout/adminLayout.jsx
+++ src/Layout/adminLayout.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Components/adminDashboard/sidebar";
-import Topbar from "../Components/adminDashboard/topbar";
+import Topbar from "../Components/adminDashboard/topBar";
 
 const AdminLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
